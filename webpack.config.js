a77382a9f0dc2a2af92505b865d3001fc841c426
robin.li <robin.li@webeye.com>
2021-05-07 15:34:24 +0800

    Development Server

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,20 @@ const { mode } = require("webpack-nano/argv");
 const {
     MiniHtmlWebpackPlugin,
 } = require("mini-html-webpack-plugin");
+const { WebpackPluginServe } = require("webpack-plugin-serve");
 
 module.exports = {
+    watch: mode === "development",
+    entry: ["./src", "webpack-plugin-serve/client"],
     mode,
     plugins: [
         new MiniHtmlWebpackPlugin({ context: { title: "Demo" } }),
+        new WebpackPluginServe({
+            port: process.env.PORT || 8080,
+            host: "127.0.0.1",
+            static: "./dist",
+            liveReload: true,
+            waitForBuild: true,
+        }),
     ],
 };
