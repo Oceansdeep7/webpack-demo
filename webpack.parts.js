af24ce117856d75df1c4a1310e34851d17fa2443
robin.li <robin.li@webeye.com>
2021-05-07 17:42:35 +0800

    Styling

--- webpack.parts.js
+++ webpack.parts.js
@@ -2,6 +2,35 @@ const { WebpackPluginServe } = require("webpack-plugin-serve");
 const {
     MiniHtmlWebpackPlugin,
 } = require("mini-html-webpack-plugin");
+const MiniCssExtractPlugin  = require("mini-css-extract-plugin")
+
+const path = require("path");
+const glob = require("glob");
+const PurgeCSSPlugin = require("purgecss-webpack-plugin");
+
+const ALL_FILES = glob.sync(path.join(__dirname, "src/*.js"));
+
+exports.eliminateUnusedCSS = () => ({
+    plugins: [
+        new PurgeCSSPlugin({
+            paths: ALL_FILES, // Consider extracting as a parameter
+            extractors: [
+                {
+                    extractor: (content) =>
+                        content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+                    extensions: ["html"],
+                },
+            ],
+        }),
+    ],
+});
+
+exports.autoprefix = () => ({
+    loader: "postcss-loader",
+    options: {
+        postcssOptions: { plugins: [require("autoprefixer")()] },
+    },
+});
 
 exports.devServer = () => ({
     watch: true,
@@ -18,3 +47,40 @@ exports.devServer = () => ({
 exports.page = ({ title }) => ({
     plugins: [new MiniHtmlWebpackPlugin({ context: { title } })],
 });
+
+exports.loadCSS = () => ({
+    module: {
+        rules: [
+            { test: /\.css$/, use: ["style-loader", "css-loader"] },
+        ],
+    },
+});
+
+exports.tailwind = () => ({
+    loader: "postcss-loader",
+    options: {
+        postcssOptions: { plugins: [require("tailwindcss")()] },
+    },
+});
+
+exports.extractCSS = ({ options = {}, loaders = [] } = {}) => {
+    return {
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: MiniCssExtractPlugin.loader, options },
+                        "css-loader",
+                    ].concat(loaders),
+                    sideEffects: true,
+                },
+            ],
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+            }),
+        ],
+    };
+};
